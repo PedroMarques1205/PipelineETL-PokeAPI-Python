1ae5c2b5eed9ccf5aa0c8b7d3622336a0b6353cf
Pedro Marques <T0281231@vli-logistica.com.br>
2025-06-10 10:42:54 -0300

    [CODE] Add API Requester class

---
+++ Code/src/common/constants_utils.py
@@ -0,0 +1,2 @@
+BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0"
+LOGGER_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
---
+++ Code/src/drivers/api_requester.py
@@ -0,0 +1,59 @@
+import requests
+import logging
+
+from typing import List, Dict
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from src.common.constants_utils import BASE_URL
+
+logger = logging.getLogger(__name__)
+
+class ApiRequester:
+
+    @staticmethod
+    def request_list_init_pokemons() -> List[Dict[str, str]]:
+        url = BASE_URL
+        logger.info("Iniciando requisição GET %s", url)
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            logger.error("Erro na requisição %s: %s", url, e, exc_info=True)
+            raise
+        else:
+            logger.info(
+                "Resposta recebida: status=%s, conteúdo=%r",
+                response.status_code,
+                response.text[:200]
+            )
+            return response.json()['results']
+
+
+    def request_pokemon_details(self) -> List[Dict]:
+        logger.info("Iniciando requisição dos detalhes dos pokemons")
+        pokemons_list = self.request_list_init_pokemons()
+        urls = [pokemon['url'] for pokemon in pokemons_list]
+
+        def fetch(url):
+            logger.info(f"Iniciando requisição para {url}")
+            try:
+                resp = requests.get(url)
+                resp.raise_for_status()
+                logger.debug(f"Requisição para {url} finalizada com status {resp.status_code}")
+                return resp.json()
+            except requests.RequestException as e:
+                logger.error(f"Erro na requisição {url}: {e}", exc_info=True)
+                return None
+
+        results = []
+        failed_count = 0
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            futures = {executor.submit(fetch, url): url for url in urls}
+            for future in as_completed(futures):
+                data = future.result()
+                if data:
+                    results.append(data)
+                else:
+                    failed_count += 1
+
+        logger.info(f"Finalizada a requisição dos detalhes: {len(results)} sucessos, {failed_count} falhas")
+        return results
